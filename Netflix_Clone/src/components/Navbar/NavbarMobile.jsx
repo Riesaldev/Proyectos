c6abb89f8947812ecb2627a1889ac446bfe1def2
RicardoEA <ricardoea@hotmail.es>
2025-04-04 08:45:47 +0100

    Add SliderVideo component and enhance NavbarMobile; implement button variants and update Tailwind config

--- Netflix_Clone/src/components/Navbar/NavbarMobile.jsx
+++ Netflix_Clone/src/components/Navbar/NavbarMobile.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Logo from "../Logo";
-import { Menu } from "lucide-react";
+import { BellRing, Menu, Search } from "lucide-react";
 import itemsNavbar from "@/utils/itemsNavbar.js";
 import cn from "@/lib/utils";
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
 
@@ -23,17 +20,23 @@ function NavbarMobile () {
                     <Menu />
                 </SheetTrigger>
                 <SheetContent side="left" className="bg-black">
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
                         {itemsNavbar.map( ( item ) => (
                             <a
                                 key={item.label}
                                 href={item.href}
-                                className={cn( "text-white text-sm font-semibold hover:text-gray-300 transition-all duration-300", item.className )}
+                                className={cn( "text-white text-sm font-semibold hover:text-gray-300 transition-all duration-300 p-3", item.className )}
                             >
                                 {item.label}
                             </a>
                         ) )}
-
+                    </div>
+                    <div className="border-[1px] border-white/70 my-5" />
+                    <div className="flex justify-between gap-6 px-3">
+                        <Search className="cursor-pointer" />
+                        <BellRing className="cursor-pointer" />
+                        {/*TODO: Add user profile */}
+                        <p>User</p>
                     </div>
                 </SheetContent>
             </Sheet>
--- Netflix_Clone/src/pages/Home.jsx
+++ Netflix_Clone/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar.jsx";
+import Navbar from "@/components/Navbar/Navbar.jsx";
+import SliderVideo from "@/components/Home/SliderVideo.jsx";
 
 export default function PageHome () {
     return (
         <>
             <div className="relative bg-zinc-900">
                 <Navbar />
+                <SliderVideo />
             </div>
         </>
     );
