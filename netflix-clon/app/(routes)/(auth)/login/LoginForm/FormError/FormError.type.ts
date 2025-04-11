14c891a75c920f85277463d6a182b2a8b79d47c8
RicardoEA <ricardoea@hotmail.es>
2025-04-11 18:54:19 +0100

    Remove unused files and components; clean up project structure

--- netflix-clon/app/(routes)/(auth)/login/LoginForm/FormError/FormError.type.ts
+++
@@ -1,3 +0,0 @@
-export type FormErrorProps = {
-    message?: string;
-};
--- netflix-clon/app/(routes)/(auth)/login/LoginForm/LoginForm.form.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const formSchema = z.object({
-    email: z.string().email({ message: 'El correo electrónico no es válido' }),
-    password: z.string().min(8, { message: 'La contraseña debe tener al menos 8 caracteres' }),
-});
-
-
