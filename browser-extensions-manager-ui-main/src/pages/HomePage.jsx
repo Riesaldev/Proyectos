18cb024956d5d9ff257137424cc5b5cc13b31b15
RicardoEA <ricardoea@hotmail.es>
2025-05-02 20:10:33 +0100

    refactor: Update HomePage layout and button styles for improved UI consistency

--- browser-extensions-manager-ui-main/src/pages/HomePage.jsx
+++ browser-extensions-manager-ui-main/src/pages/HomePage.jsx
@@ -3,42 +3,44 @@ import data from "../../data.json";
 import { useState } from "react";
 
 const HomePage = () => {
-    const [ activeButton, setActiveButton ] = useState( "all" ); // Por defecto, "all" está seleccionado
+    const [ activeButton, setActiveButton ] = useState( "all" );
 
     const handleButtonClick = ( buttonName ) => {
         setActiveButton( buttonName );
     };
 
     return (
-        <div className="mx-auto px-8 py-12 bg-slate-200 flex flex-col gap-4 min-h-screen items-center">
-            <h1 className="text-4xl font-bold">Extension List</h1>
-            <section className="w-1/2 flex justify-between items-center px-4 py-4">
-                <button
-                    className={`border px-4 py-1 rounded-2xl ${ activeButton === "all" ? "bg-red-500 text-white" : "bg-white"
-                        }`}
-                    type="button"
-                    onClick={() => handleButtonClick( "all" )}
-                >
-                    All
-                </button>
-                <button
-                    className={`border px-4 py-1 rounded-2xl ${ activeButton === "active" ? "bg-red-500 text-white" : "bg-white"
-                        }`}
-                    type="button"
-                    onClick={() => handleButtonClick( "active" )}
-                >
-                    Active
-                </button>
-                <button
-                    className={`border px-4 py-1 rounded-2xl ${ activeButton === "inactive" ? "bg-red-500 text-white" : "bg-white"
-                        }`}
-                    type="button"
-                    onClick={() => handleButtonClick( "inactive" )}
-                >
-                    Inactive
-                </button>
-            </section>
-            <section>
+        <div className=" bg-slate-200 min-h-screen flex flex-col items-center pt-10 max-xl:min-w-[400px] xl:w-full xl:min-w-[800px]">
+            <div className="flex flex-col max-xl:flex-col xl:flex-row justify-between items-center rounded-2xl py-4 mb-4 max-xl:w-1/2 max-xl:min-w-[400px] xl:w-1/2 xl:min-w-[800px]">
+                <h1 className="text-4xl font-bold">Extension List</h1>
+                <section className="flex gap-4 mt-4 xl:mt-0">
+                    <button
+                        className={`border px-4 py-1 rounded-full ${ activeButton === "all" ? "bg-red-500 text-white" : "bg-white"
+                            }`}
+                        type="button"
+                        onClick={() => handleButtonClick( "all" )}
+                    >
+                        All
+                    </button>
+                    <button
+                        className={`border px-4 py-1 rounded-full ${ activeButton === "active" ? "bg-red-500 text-white" : "bg-white"
+                            }`}
+                        type="button"
+                        onClick={() => handleButtonClick( "active" )}
+                    >
+                        Active
+                    </button>
+                    <button
+                        className={`border px-4 py-1 rounded-full ${ activeButton === "inactive" ? "bg-red-500 text-white" : "bg-white"
+                            }`}
+                        type="button"
+                        onClick={() => handleButtonClick( "inactive" )}
+                    >
+                        Inactive
+                    </button>
+                </section>
+            </div>
+            <section className="flex flex-wrap justify-center gap-4 max-xl:w-1/2 max-xl:min-w-[400px] xl:w-1/2 p-4">
                 {data.map( ( item, index ) => (
                     <div key={index}>
                         <Item item={item} />
