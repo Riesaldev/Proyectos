fac05ee52c195df15c93e373f20fe5bd60f0f778
RicardoEA <ricardoea@hotmail.es>
2025-05-02 11:57:11 +0100

    feat: Replace Link components with Button for profile creation and management; enhance UI with improved styling

--- netflix-clon/client/src/pages/ProfilesPage.jsx
+++ netflix-clon/client/src/pages/ProfilesPage.jsx
@@ -2,8 +2,8 @@ import ProfileCards from '@/components/Cards/ProfileCards';
 import Logo from '@/components/Logo';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
-import useAuthContext from '@/hooks/useAuthContext'; // Importamos el hook del contexto de autenticación
+import useAuthContext from '@/hooks/useAuthContext';
+import { Button } from '@/components/ui/button';
 
 const { VITE_API_URL } = import.meta.env;
 
@@ -50,22 +50,35 @@ const ProfilesPage = () => {
             <div className="bg-gray-900 w-screen h-17 flex items-center ml-4">
                 <Logo />
             </div>
-            <div className="bg-black h-1/2 w-auto mt-16 items-center mx-auto rounded-lg shadow-lg flex flex-col">
+            <div className="bg-black opacity-80 h-1/2 w-auto mt-16 items-center mx-auto rounded-lg flex flex-col">
                 <h1 className="text-3xl text-center mt-8">¿Quién eres? Elige tu perfil</h1>
                 <div className="flex flex-row gap-8 items-start justify-center my-18">
                     {profiles.map( ( profile ) => (
                         <ProfileCards key={profile.profileId} profile={profile} />
                     ) )}
-                    <div className="flex flex-col items-center justify-center w-24 h-24 bg-gray-800 rounded-full cursor-pointer hover:bg-gray-700 transition duration-300 ease-in-out">
-                        <Link to="/create" className="items-center justify-center flex flex-col">
-                            <Logo isLink={false} />
-                            <p>Añadir perfil</p>
-                        </Link>
+                    <div className="flex flex-col items-center justify-center w-24 h-24 rounded-full hover:bg-gray-300 transition duration-300 ease-in-out">
+                        <Button
+                            className="items-center justify-center flex flex-col  bg-center bg-cover w-24 h-24 rounded-full cursor-pointer "
+                            variant="primary"
+                            size="icon"
+                            onClick={() => {
+                                window.location.href = '/create';
+                            }}
+                        >
+                            <p className='text-8xl font-black  text-red-900 hover:text-gray-600 '>+</p>
+                        </Button>
                     </div>
                 </div>
-                <Link to="/manage" className="border">
-                    <p className="mx-6 my-2 text-xl">Administrar Perfiles</p>
-                </Link>
+                <Button
+                    variant="secondary"
+                    className="w-1/5 mx-auto my-6 shadow-red-500 bg-gray-600 text-white text-lg font-medium tracking-wider hover:bg-gray-600 hover:text-gray-900 cursor-pointer"
+                    onClick={() => {
+                        window.location.href = '/manage';
+                    }
+                    }
+                >
+                    Administrar perfiles
+                </Button>
             </div>
         </div>
     );
