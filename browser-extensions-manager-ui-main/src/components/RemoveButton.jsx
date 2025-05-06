91ddd65a25b75b92a1d7e09a592adda5a0a65fca
RicardoEA <ricardoea@hotmail.es>
2025-05-06 13:18:27 +0100

    style: Enhance RemoveButton hover effects for better accessibility and dark mode support

--- browser-extensions-manager-ui-main/src/components/RemoveButton.jsx
+++ browser-extensions-manager-ui-main/src/components/RemoveButton.jsx
@@ -5,7 +5,7 @@ const RemoveButton = () => {
         <div>
             <button
                 type="button"
-                className="bg-white font-semibold py-1 px-3 rounded-full border-2 "
+                className="bg-white font-semibold py-1 px-3 rounded-full border-2 hover:bg-red-700 hover:text-white hover:border-red-700 dark:hover:border-red-700"
             >
                 Remove
             </button>
