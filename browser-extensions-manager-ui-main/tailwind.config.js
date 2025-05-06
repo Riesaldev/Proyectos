797b620174a1ab5ee3580531f8aca07754371d2e
RicardoEA <ricardoea@hotmail.es>
2025-05-06 13:17:18 +0100

    feat: Add color palette to Tailwind configuration for enhanced theming options

--- browser-extensions-manager-ui-main/tailwind.config.js
+++ browser-extensions-manager-ui-main/tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [ './index.html', './src/**/*.{js,ts,jsx,tsx}' ],
     theme: {
         extend: {
             colors: {
-            },
-            fontFamily: {
-
+                neutral: {
+                    900: 'hsl(227, 75%, 14%)',
+                    800: 'hsl(226, 25%, 17%)',
+                    700: 'hsl(225, 23%, 24%)',
+                    600: 'hsl(226, 11%, 37%)',
+                    300: 'hsl(0, 0%, 78%)',
+                    200: 'hsl(217, 61%, 90%)',
+                    100: 'hsl(0, 0%, 93%)',
+                    0: 'hsl(200, 60%, 99%)',
+                },
+                red: {
+                    400: 'hsl(3, 86%, 64%)',
+                    500: 'hsl(3, 71%, 56%)',
+                    700: 'hsl(3, 77%, 44%)',
+                },
             },
         },
     },
