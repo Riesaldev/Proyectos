15472efc0427bf95b849ea1a33c40bdc7ba28a27
RicardoEA <ricardoea@hotmail.es>
2025-05-06 13:18:39 +0100

    feat: Refactor SwitchButton to accept isActive and onToggle props for improved functionality

--- browser-extensions-manager-ui-main/src/components/SwitchButton.jsx
+++ browser-extensions-manager-ui-main/src/components/SwitchButton.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react';
-
-const SwitchButton = () => {
-    const [ isOn, setIsOn ] = useState( false );
-
-    const handleToggle = () => {
-        setIsOn( !isOn );
-    }
+const SwitchButton = ( { isActive, onToggle } ) => {
     return (
         <div className='h-8 w-10 flex items-center'>
             <button
-                onClick={handleToggle}
-                className={`w-10 h-[22px] rounded-full flex items-center p-1 transition-colors duration-300 ${ isOn ? 'bg-red-500' : 'bg-gray-300' }`}
+                onClick={onToggle}
+                className={`w-10 h-[22px] rounded-full flex items-center p-1 transition-colors duration-300 ${ isActive ? 'bg-red-500' : 'bg-gray-300 hover:bg-red-500' }`}
             >
-                <div className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${ isOn ? 'translate-x-4' : 'translate-x-0' }`} />
+                <div className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${ isActive ? 'translate-x-4' : 'translate-x-0 hover:translate-x-4' }`} />
             </button>
         </div>
     );
-}
+};
 
 export default SwitchButton;
