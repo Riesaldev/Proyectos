dfef799810c7949d548cb7c3f20da3e91cd1eb19
RicardoEA <ricardoea@hotmail.es>
2025-05-06 13:18:06 +0100

    feat: Update Item component to include onToggle prop and enhance functionality

--- browser-extensions-manager-ui-main/src/components/Cards-item.jsx
+++ browser-extensions-manager-ui-main/src/components/Cards-item.jsx
@@ -1,28 +1,22 @@
 import SwitchButton from "./SwitchButton";
 import RemoveButton from "./RemoveButton";
 
-
-
-const Item = ( { item } ) => {
-
+const Item = ( { item, onToggle } ) => {
     return (
-        <>
-            <div className="rounded-2xl bg-white shadow-md p-4 w-[350px] h-[200px] flex flex-col justify-between">
-
-                <div className="flex items-start">
-                    <img src={item.logo} alt={item.name} />
-                    <div className="ml-4 flex flex-col">
-                        <h2 className="font-bold">{item.name}</h2>
-                        <p>{item.description}</p>
-                    </div>
-                </div>
-                <div className="flex justify-between items-center mt-4">
-                    <RemoveButton />
-                    <SwitchButton />
+        <div className="cards rounded-2xl bg-white shadow-md p-4 w-[400px] h-[200px] flex flex-col justify-between">
+            <div className="flex items-start">
+                <img src={item.logo} alt={item.name} />
+                <div className="ml-4 flex flex-col">
+                    <h2 className="font-bold">{item.name}</h2>
+                    <p>{item.description}</p>
                 </div>
             </div>
-        </>
+            <div className="flex justify-between items-center mt-4">
+                <RemoveButton />
+                <SwitchButton isActive={item.isActive} onToggle={onToggle} />
+            </div>
+        </div>
     );
-}
+};
 
 export default Item;
