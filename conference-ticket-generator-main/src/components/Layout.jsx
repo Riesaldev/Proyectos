7c7bdf05b7ae073cc486c9651c43559003af4333
RicardoEA <ricardoea@hotmail.es>
2025-05-07 12:56:28 +0100

    feat: update Layout component to accept children and improve styling

--- conference-ticket-generator-main/src/components/Layout.jsx
+++ conference-ticket-generator-main/src/components/Layout.jsx
@@ -1,16 +1,20 @@
+import FormDesc from "./FormDesc";
 
-const Layout = () => {
+const Layout = ( { children } ) => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-[url('../../assets/images/background-mobile.png')] bg-cover bg-no-repeat bg-center">
-            <div className="bg-[url('../../assets/images/pattern-lines.svg')] bg-no-repeat bg-center w-screen h-[700px] bg-size-[995px] absolute top-0 left-7 z-0">
-                <div>
-                    <img src="../../assets/images/pattern-squiggly-line-top.svg" alt="top-line" className="size-[110px] absolute left-60" />
-                    <img src="../../assets/images/logo-full.svg" alt="Logo" className="absolute top-[-40px] left-[5rem] z-10 size-40" />
-                    <img src="../../assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg" alt="bottom-line" className=" absolute top-[730px] right-[28px]" />
+        <div className="h-screen bg-[url('../../assets/images/background-mobile.png')] bg-cover bg-center max-w-[375px] max-h-[812px] relative  justify-center  ">
+            <div className="bg-[url('../../assets/images/pattern-lines.svg')] w-screen h-[700px] bg-size-[995px]">
+                <div className="flex flex-col items-center h-full relative">
+                    <div className="flex flex-row h-full relative place-items-center -top-10 left-15 justify-center w-auto">
+                        <img src="../../assets/images/logo-full.svg" alt="Logo" className="size-40 " />
+                        <img src="../../assets/images/pattern-squiggly-line-top.svg" alt="top-line" className="size-[110px] " />
+                    </div>
+                    {children}
+                    <img src="../../assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg" alt="bottom-line" className="relative -left-17 -top-27 w-60" />
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Layout;
