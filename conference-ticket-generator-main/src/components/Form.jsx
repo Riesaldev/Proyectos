d71827271441f0432159d3513426efa3b54414a2
RicardoEA <ricardoea@hotmail.es>
2025-05-08 16:35:38 +0100

    feat: refactor Form and FormPage components to enhance structure and styling

--- conference-ticket-generator-main/src/components/Form.jsx
+++ conference-ticket-generator-main/src/components/Form.jsx
@@ -2,29 +2,8 @@
 
 const Form = () => {
     return (
-        <div className="flex flex-col gap-4 relative w-full">
-            <div className="flex flex-col">
-                <p>Upload Avatar</p>
-                <div id="drop" onDrop="dropHandler(e); " className="border-[1px] border-dashed border-gray-300 rounded-lg flex flex-col items-center justify-center h-32 gap-5">
-                    <div className="flex items-center justify-center w-12 h-12  backdrop-blur-4xl backdrop-brightness-150 backdrop-contrast-70 rounded-lg mb-2 shadow-xs shadow-gray-900">
-                        <img src="../../assets/images/icon-upload.svg" alt="drop" />
-                    </div>
-                    <p className="text-xs">Drag and drop or click to upload</p>
-                </div>
-            </div>
-            <div className="flex flex-col gap-1">
-                <label>Full Name</label>
-                <input type="text" className="border-[1px] rounded-lg h-10" />
-            </div>
-            <div className="flex flex-col gap-1">
-                <label>Email Address</label>
-                <input type="email" placeholder="Enter your email address" className="border-[1px] rounded-lg h-10" />
-            </div>
-            <div className="flex flex-col gap-1">
-                <label>GitHub Username</label>
-                <input type="text" placeholder="Enter your GitHub username" className="border-[1px] rounded-lg h-10" />
-            </div>
-            <button type="submit" className="bg-orange-700 rounded-xl h-10 text-black">Generate My Ticket</button>
+        <div>
+            <h1>Formulario</h1>
         </div>
     );
 }
--- conference-ticket-generator-main/src/pages/FormPage.jsx
+++ conference-ticket-generator-main/src/pages/FormPage.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import FormDesc from '../components/FormDesc';
-import Layout from '../components/Layout';
-import Form from '../components/Form';
+import Form from "../components/Form";
+
+
 
 const FormPage = () => {
     return (
-        <div >
-            <Layout>
-                <FormDesc>
+        <div className="bg-[url(../../public/assets/images/background-mobile.png)] bg-no-repeat bg-center w-max-[375px] h-screen flex flex-col items-center justify-start absolute">
+            <div className="w-max-[375px] h-full w-full flex flex-col justify-start bg-[url(../../public/assets/images/pattern-lines.svg)]  bg-no-repeat bg-top-right  bg-[length:260%]">
+                <div className="w-max-[375px] h-40  flex flex-row items-baseline justify-end">
+                    <img src="../../public/assets/images/logo-full.svg" alt="logo"
+                        className=" w-56 h-54" />
+                    <img src="../../public/assets/images/pattern-squiggly-line-top.svg" alt="line-top" className="w-48 h-48" />
+                </div>
+                <div className="w-max-[375px] h-40 flex flex-row items-center justify-center">
+
                     <Form />
-                </FormDesc>
-            </Layout >
-        </div >
+                </div>
+            </div>
+            <div className="w-max-[375px] w-[575px] flex items-end">
+                <img src="../../public/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg" alt="line-bottom" className="h-3/4 self-end" />
+            </div>
+        </div>
     );
 };
 export default FormPage;
