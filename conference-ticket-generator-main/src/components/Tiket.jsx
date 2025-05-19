b261f3fc2a15dc43d95eb54a39bc3a30d544cd01
RicardoEA <ricardoea@hotmail.es>
2025-05-19 10:18:08 +0100

    feat: agregar valores predeterminados a los campos de Ticket para mejorar la experiencia del usuario

--- conference-ticket-generator-main/src/components/Tiket.jsx
+++ conference-ticket-generator-main/src/components/Tiket.jsx
@@ -4,13 +4,13 @@
 
 
 const Ticket = () => {
-  const name = localStorage.getItem( "name" );
-  const email = localStorage.getItem( "email" );
-  const git = localStorage.getItem( "git" );
-  const avatar = localStorage.getItem( "avatar" );
-  const date = localStorage.getItem( "date" );
-  const localization = localStorage.getItem( "localization" );
-  const Ticketcode = localStorage.getItem( "Ticketcode" );
+  const name = localStorage.getItem( "name" ) || "Jonatan Kristof";
+  const email = localStorage.getItem( "email" ) || "jonatan@email.com";
+  const git = localStorage.getItem( "git" ) || "@jonatanKristof0101";
+  const avatar = localStorage.getItem( "avatar" ) || "";
+  const date = localStorage.getItem( "date" ) || "Jan 31, 2025";
+  const localization = localStorage.getItem( "localization" ) || "Austin, TX";
+  const Ticketcode = localStorage.getItem( "Ticketcode" ) || "#01609";
 
   return (
     <div className="flex flex-col items-center min-h-screen w-full gap-8 py-8 px-4 pb-16">
@@ -19,11 +19,11 @@ const Ticket = () => {
         {/* TicketPage Title */}
         <div className="flex flex-col items-center w-full gap-6">
           <div className="">
-            <h1 className="text-3xl font-bold text-neutral-100">Congrats, <span className="text-orange-500">{name} Jonatan Kristof</span>! Your ticket is ready.</h1>
+            <h1 className="text-3xl font-bold text-neutral-100">Congrats, <span className="text-orange-500">{name}</span>! Your ticket is ready.</h1>
           </div>
           {/* TicketPage Details */}
           <div className=" flex flex-col gap-2 w-3/4">
-            <p className="text-neutral-400 text-xl ">We've emailed your ticket to <span className="text-orange-400 font-light ">{email}jonatan@email.com</span> and will send updates in the run up to the event.</p>
+            <p className="text-neutral-400 text-xl ">We've emailed your ticket to <span className="text-orange-400 font-light ">{email}</span> and will send updates in the run up to the event.</p>
           </div>
         </div>
         {/* TicketPage Image */}
@@ -39,7 +39,7 @@ const Ticket = () => {
               <div className="flex flex-col h-3/4 w-3/5 justify-around ">
                 <div className="flex flex-col h-14 ">
                   <img src="../../public/assets/images/logo-full.svg" alt="logo" className="w-44" />
-                  <p className="text-neutral-400 text-xs">{date}Jan 31, 2025 / {localization} Austin, TX</p>
+                  <p className="text-neutral-400 text-xs">{date} / {localization}</p>
                 </div>
                 {/*User information*/}
                 <div className="flex flex-row w-full gap-2">
@@ -47,11 +47,11 @@ const Ticket = () => {
                   <img src="../../public/assets/images/image-avatar.jpg" alt="avatar" className="w-10 h-10 rounded-lg" />
                   {/*user name */}
                   <div className="flex flex-col items-start">
-                    <h3 className="text-neutral-100 text-md font-light">{name}Jonatan Kristof</h3>
+                    <h3 className="text-neutral-100 text-md font-light">{name}</h3>
                     {/*GitHub username */}
                     <div className="flex flex-row items-center">{avatar}
                       <img src="../../public/assets/images/icon-github.svg" alt="confirmation code" className="w-4 h-3" />
-                      <p className="text-neutral-400 text-xs">{git}@jonatanKristof0101</p>
+                      <p className="text-neutral-400 text-xs">{git}</p>
                     </div>
                   </div>
                 </div>
@@ -60,7 +60,7 @@ const Ticket = () => {
               {/*Ticket code */}
               <div className="flex flex-col items-center rotate-90 ">
 
-                <p className="text-neutral-500 text-3xl">{Ticketcode}#01609</p>
+                <p className="text-neutral-500 text-3xl">{Ticketcode}</p>
               </div>
             </div>
           </div>
