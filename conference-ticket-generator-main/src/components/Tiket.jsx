02a37df6edd8a4a475b2153f936e8f31f2efee03
RicardoEA <ricardoea@hotmail.es>
2025-05-19 09:53:32 +0100

    feat: actualizar la estructura y estilos de TicketPage para mejorar la presentación y la información del usuario

--- conference-ticket-generator-main/src/components/Tiket.jsx
+++ conference-ticket-generator-main/src/components/Tiket.jsx
@@ -4,44 +4,63 @@
 
 
 const Ticket = () => {
-  const userName = localStorage.getItem( "userName" );
-  const userEmail = localStorage.getItem( "userEmail" );
+  const name = localStorage.getItem( "name" );
+  const email = localStorage.getItem( "email" );
+  const git = localStorage.getItem( "git" );
+  const avatar = localStorage.getItem( "avatar" );
+  const date = localStorage.getItem( "date" );
+  const localization = localStorage.getItem( "localization" );
+  const Ticketcode = localStorage.getItem( "Ticketcode" );
+
   return (
     <div className="flex flex-col items-center min-h-screen w-full gap-8 py-8 px-4 pb-16">
 
-      <div className="gap-4 flex flex-col w-full justify-center text-center items-center">
+      <div className="flex flex-col w-full h-2/3 gap-24 items-center">
         {/* TicketPage Title */}
-        <div className="Title flex flex-col gap-2 w-3/4">
-          <h1 className="text-3xl font-bold text-neutral-100">Congrats, <span>{userName}</span>! Your ticket is ready.</h1>
-        </div>
-        {/* TicketPage Details */}
-        <div className=" flex flex-col gap-2 w-3/4">
-          <p className="text-neutral-400 text-xl ">We've emailed your ticket to <span>{userEmail}</span> and will send updates in the run up to the event.</p>
+        <div className="flex flex-col items-center w-full gap-6">
+          <div className="">
+            <h1 className="text-3xl font-bold text-neutral-100">Congrats, <span className="mask-l-from-75%">{name} Jonatan Kristof</span>! Your ticket is ready.</h1>
+          </div>
+          {/* TicketPage Details */}
+          <div className=" flex flex-col gap-2 w-3/4">
+            <p className="text-neutral-400 text-xl ">We've emailed your ticket to <span className="text-orange-400 font-light ">{email}jonatan@email.com</span> and will send updates in the run up to the event.</p>
+          </div>
         </div>
         {/* TicketPage Image */}
-        <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center  w-full">
           <div className="relative w-full">
             {/* Ticket Image */}
             <img src="../../public/assets/images/pattern-ticket.svg" alt="ticket" className="w-full h-auto" />
+
             {/* Ticket content */}
-            <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
+            <div className="absolute top-0 left-4 w-full h-full flex items-center justify-between gap-4 ">
+
               {/*event information*/}
-              <div className="flex flex-col items-center gap-2">
-                <h2 className="text-2xl font-bold text-neutral-100">Logo</h2>
-                <p className="text-neutral-400 text-lg">date</p>
+              <div className="flex flex-col h-3/4 items-center justify-around ">
+                <div className="flex flex-col h-14 ">
+                  <img src="../../public/assets/images/logo-full.svg" alt="logo" className="w-44" />
+                  <p className="text-neutral-400 text-xs">{date}Jan 31, 2025 / {localization} Austin, TX</p>
+                </div>
+                {/*User information*/}
+                <div className="flex flex-row w-full gap-2">
+                  {/*avatar */}
+                  <img src="../../public/assets/images/image-avatar.jpg" alt="avatar" className="w-10 h-10 rounded-lg" />
+                  {/*user name */}
+                  <div className="flex flex-col items-start">
+                    <h3 className="text-neutral-100 text-md font-light">{name}Jonatan Kristof</h3>
+                    {/*GitHub username */}
+                    <div className="flex flex-row items-center">{avatar}
+                      <img src="../../public/assets/images/icon-github.svg" alt="confirmation code" className="w-4 h-3" />
+                      <p className="text-neutral-400 text-xs">{git}@jonatanKristof0101</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              {/*User information*/}
-              <div className="flex flex-col items-center gap-2">
-                {/*avatar */}
-                <img src="../../public/assets/images/avatar.png" alt="avatar" className="w-16 h-16 rounded-full" />
-                {/*user name */}
-                <h3 className="text-neutral-100 text-lg">{userName}</h3>
-                {/*confirmation code */}
-                <p className="text-neutral-400 text-sm">Confirmation code</p>
-                <p className="text-neutral-100 text-lg">123456789</p>
-                {/*Ticket code */}
-                <p className="text-neutral-400 text-sm">Ticket code</p>
-                <p className="text-neutral-100 text-lg">123456789</p>
+
+              {/*Ticket code */}
+              <div className="flex flex-col items-center rotate-90 ">
+
+                <p className="text-neutral-500 text-3xl">{Ticketcode}#01609</p>
               </div>
             </div>
           </div>
