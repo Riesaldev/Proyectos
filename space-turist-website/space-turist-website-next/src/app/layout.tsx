4fbed81b154e762c264e6c95cbad1fe6de66ca5a
RicardoEA <ricardoea@hotmail.es>
2025-05-20 12:03:04 +0100

    fix: corregir la ruta del ícono y mejorar la estructura del layout

--- space-turist-website/space-turist-website-next/src/app/layout.tsx
+++ space-turist-website/space-turist-website-next/src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -7,8 +8,10 @@ export const metadata: Metadata = {
   title: "Space Turist",
   description: "this is a website exercise for practice with next.js, typescript and tailwind doing a website for a space travel agency",
   icons: {
-    icon: "/space-turist-website-next/public/favicon-32x32.png",
+    icon: "/shared/logo.svg",
   },
+
+
 };
 
 export default function RootLayout({
@@ -18,11 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body
-      >
+      <head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Bellefair&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&display=swap');
+        </style>
+      </head>
+      <body className="bg-[url(../../public/home/background-home-mobile.jpg)] bg-cover bg-no-repeat bg-center h-screen w-screen  items-center justify-between sm:bg-[url(../../public/home/background-home-tablet.jpg)] md:bg-[url(../../public/home/background-home-desktop.jpg)] ">
         <Header />
         {children}
       </body>
     </html>
   );
+
 }
