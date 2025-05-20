f28b9a93782aeb59e0457271d8481c41cd585288
RicardoEA <ricardoea@hotmail.es>
2025-05-20 12:09:35 +0100

    fix: corregir la estructura del párrafo y mejorar la semántica en el componente Home

--- space-turist-website/space-turist-website-next/src/app/page.tsx
+++ space-turist-website/space-turist-website-next/src/app/page.tsx
@@ -15,11 +15,13 @@ export default function Home() {
         <span className="text-7xl font-light font-[bellefair] uppercase">
           Space
         </span>
-        <p className="text-base/7 text-center text-balance font-light sm:text-base text-white/60 font-[barlow] px-4 py-4 border border-amber-700 ">
+        <section className="items-center sm:items-center max-w-[500px] w-svw mt-4">
+        <p className="text-base/7 text-center font-light sm:text-base text-white/60 font-[barlow] p-4 border border-amber-700 ">
           Let’s face it; if you want to go to space, you might as well genuinely go to
           outer space and not hover kind of on the edge of it. Well sit back, and relax
           because we’ll give you a truly out of this world experience!
         </p>
+        </section>
         <div className="flex flex-col items-center sm:items-center h-full w-full gap-10 p-8 mt-42">
           <ButtonExplore onClick={() => alert("Exploring space!")} />
         </div>
