0d50c6090e0107a3b0609c2b941eb96e46bbe75d
RicardoEA <ricardoea@hotmail.es>
2025-05-23 18:45:03 +0100

    feat: agregar clase Spaceship para manejar la carga y el control del modelo de nave espacial

---
+++ space-threejs/src/core/spaceship.ts
@@ -0,0 +1,41 @@
+
+import type { Object3D, Scene } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import type InputController from './input.controller.js';
+
+export default class Spaceship {
+  private readonly gltfloader = new GLTFLoader();
+  private declare model: Object3D;
+  private readonly speed: number = 0.2;
+  private readonly rotationSpeed: number = 0.05;
+
+  constructor(
+    private readonly scene: Scene,
+    private readonly inputController: InputController,
+    private readonly scale: number) {}
+
+  public loadModel(): void {
+
+    this.gltfloader.load('/spaceship.glb', (gltf: GLTF) => {
+      this.model = gltf.scene;
+      this.model.scale.set(this.scale, this.scale, this.scale);
+      this.scene.add(this.model);
+    });
+  }
+  public update(): void {
+    if(!this.model) return;
+    if (this.inputController.isPressed('KeyW')||this.inputController.isPressed('ArrowUp')) {
+      this.model.translateZ(this.speed);      
+    }
+    if (this.inputController.isPressed('KeyS')||this.inputController.isPressed('ArrowDown')) {
+      this.model.translateZ(-this.speed);
+    }
+    if (this.inputController.isPressed('KeyA')||this.inputController.isPressed('ArrowLeft')) {
+      this.model.rotateY(this.rotationSpeed);
+    }
+    if (this.inputController.isPressed('KeyD')||this.inputController.isPressed('ArrowRight')) {
+      this.model.rotateY(-this.rotationSpeed);
+    }
+  }
+}
