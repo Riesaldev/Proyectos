74c62ec51e04215c999797ac53ee0a0ac5f57471
RicardoEA <ricardoea@hotmail.es>
2025-05-26 12:25:15 +0100

    feat: agregar soporte para actualizaciones en vivo de efectos

--- Add-on_Blender/properties.py
+++ Add-on_Blender/properties.py
@@ -33,3 +33,29 @@ def unregister():
     del bpy.types.Scene.motionfx_settings
     bpy.utils.unregister_class(MotionFXSettings)
 
+live_update = BoolProperty(
+    name="Live Update",
+    description="Enable live updates for effects",
+    default=True,
+    updates=lambda self, ctx: self.update_live_parameters(ctx)
+)
+
+def update_live_parameters(self, context):
+    if self.live_update:
+        # Update parameters in real-time
+        pass
+    else:
+        # Disable live updates
+        pass
+def register_live_update():
+    bpy.types.Scene.live_update = live_update
+def unregister_live_update():
+    del bpy.types.Scene.live_update
+def register():
+    register()
+    register_live_update()
+def unregister():
+    unregister()
+    unregister_live_update()
+
+    bpy.ops.wm.save_userpref(type = 'DRAW_WIN_SWAP', iterations = 1)
