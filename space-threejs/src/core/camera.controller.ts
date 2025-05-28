e7fcae6f908558035f9938addfc4c9850482eaf9
RicardoEA <ricardoea@hotmail.es>
2025-05-28 12:23:35 +0100

    feat: añadir controlador de cámara para seguir el modelo de la nave espacial

---
+++ space-threejs/src/core/camera.controller.ts
@@ -0,0 +1,19 @@
+import { PerspectiveCamera, Vector3 } from "three";
+import Spaceship  from "./spaceship";
+
+export class CameraController {
+  private readonly offset: Vector3 = new Vector3(0, 4, -8);
+
+  constructor(private readonly perspectiveCamera: PerspectiveCamera, private readonly spaceship: Spaceship) {}
+
+  public update(): void {
+    if (!this.spaceship.model)
+      return;
+
+    const rotateOffset = this.offset.clone().applyQuaternion(this.spaceship.model.quaternion);
+    const desiredPosition = this.spaceship.model.position.clone().add(rotateOffset);
+
+    this.perspectiveCamera.position.lerp(desiredPosition, 0.03);
+    this.perspectiveCamera.lookAt(this.spaceship.model.position);
+  }
+}
