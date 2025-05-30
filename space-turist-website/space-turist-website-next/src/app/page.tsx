8565ddd284805564c267412a3854ec40576bb8c5
RicardoEA <ricardoea@hotmail.es>
2025-05-30 12:55:29 +0100

    refactor: mejorar la estructura y el estilo del componente principal de la página

--- space-turist-website/space-turist-website-next/src/app/page.tsx
+++ space-turist-website/space-turist-website-next/src/app/page.tsx
@@ -7,24 +7,26 @@ export default function Home() {
 
 
   return (
-    <main className="flex flex-col items-center pt-4 text-white">
-      <div className="flex flex-col items-center sm:items-center h-full w-full gap-6 mt-4">
-        <h1 className="font-extralight  text-white/60 font-[barlow] uppercase">
-          So, you want to travel to
+    <main className="text-white flex flex-col justify-center min-h-screen">
+      <div className="flex flex-col items-center sm:flex-row h-full ">
+      <div className="flex flex-col  h-full w-[540px] gap-10 mt-4 sm:ml-60 items-center">
+        <h1 className="text-white/60 font-[barlow] uppercase text-lg  tracking-widest">
+        So, you want to travel to
         </h1>
-        <span className="text-7xl font-light font-[bellefair] uppercase">
-          Space
+        <span className="text-9xl font-[bellefair] uppercase">
+        Space
         </span>
-        <section className="items-center sm:items-center max-w-[500px] w-svw mt-4">
-        <p className="text-base/7 text-center font-light sm:text-base text-white/60 font-[barlow] p-4 border border-amber-700 ">
+        <section className="max-w-[480px] w-svw">
+        <p className="text-base font-light text-white/60 font-[barlow] leading-loose">
           Let’s face it; if you want to go to space, you might as well genuinely go to
           outer space and not hover kind of on the edge of it. Well sit back, and relax
           because we’ll give you a truly out of this world experience!
         </p>
         </section>
-        <div className="flex flex-col items-center sm:items-center h-full w-full gap-10 p-8 mt-42">
-          <ButtonExplore onClick={() => alert("Exploring space!")} />
-        </div>
+      </div>
+      <div className="flex flex-col items-center sm:items-center h-full w-full mt-12 pt-8 sm:mt-0">
+        <ButtonExplore onClick={() => alert("Exploring space!")} />
+      </div>
       </div>
     </main>
   );
