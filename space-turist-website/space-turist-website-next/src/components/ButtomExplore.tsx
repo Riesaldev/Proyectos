0711675d0753712c08d6151b8a431bf8c800e22f
RicardoEA <ricardoea@hotmail.es>
2025-05-30 12:55:36 +0100

    feat: mejorar el estilo del botón de exploración con nuevas clases y efectos de hover

--- space-turist-website/space-turist-website-next/src/components/ButtomExplore.tsx
+++ space-turist-website/space-turist-website-next/src/components/ButtomExplore.tsx
@@ -7,7 +7,7 @@ type ButtonExploreProps = {
 export default function ButtonExplore({ onClick }: ButtonExploreProps) {
   return (
     <button
-      className="bg-white text-black h-30 w-30 p-6 rounded-full font-[bellefair]"
+      className="bg-white text-3xl text-gray-800 h-50 w-50 p-6 rounded-full font-[bellefair] hover:shadow-[0_0_0_80px_rgba(255,255,255,0.1)] hover:text-gray-400 transition duration-300"
       title="explore"
       type="button"
       onClick={onClick}
