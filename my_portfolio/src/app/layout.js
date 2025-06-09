b7abc5f42b5b48e60873003d78b5143300637ffb
RicardoEA <ricardoea@hotmail.es>
2025-06-09 18:50:17 +0100

    feat: añadir componente Header en el layout y mejorar estilos de la página principal

--- my_portfolio/src/app/layout.js
+++ my_portfolio/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💼</text></svg>" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
