da12b81aa49d2532ac90c2a72f939b6c8507e834
RicardoEA <ricardoea@hotmail.es>
2025-06-10 09:48:47 +0100

    feat: actualizar estilos del menú móvil y mejorar la legibilidad del texto

--- my_portfolio/src/components/Menu.jsx
+++ my_portfolio/src/components/Menu.jsx
@@ -21,18 +21,18 @@ const MobileMenu = () => {
           alt="Menu" 
           width={40} 
           height={40}
-          className="fill-white h-10 w-10"
+          className="h-10 w-10 text-[#fddbff] "
         />
       </button>
       {menuOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50">
-          <Link href="#about" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" onClick={toggleMenu}>
+        <div className="absolute right-0 mt-2 w-48 bg-[#8e3796] rounded-md shadow-lg py-1 z-50 text-[#fddbff] text-xl">
+          <Link href="#about" className="block px-4 py-2  hover:text-gray-100 hover:underline" onClick={toggleMenu}>
             About
           </Link>
-          <Link href="#projects" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" onClick={toggleMenu}>
+          <Link href="#projects" className="block px-4 py-2  hover:text-gray-100 hover:underline" onClick={toggleMenu}>
             Projects
           </Link>
-          <Link href="#contact" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" onClick={toggleMenu}>
+          <Link href="#contact" className="block px-4 py-2  hover:text-gray-100 hover:underline" onClick={toggleMenu}>
             Contact
           </Link>
         </div>
