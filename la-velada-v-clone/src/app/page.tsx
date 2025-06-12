c42bc0945aa786af0232e9673b2518e3894f7eea
RicardoEA <ricardoea@hotmail.es>
2025-06-12 20:35:42 +0100

    feat: simplificar la estructura del componente Home y ajustar la imagen de fondo

--- la-velada-v-clone/src/app/page.tsx
+++ la-velada-v-clone/src/app/page.tsx
@@ -1,9 +1,15 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-[400px]">
-      <div className=" bg-[url('/public/images/hero.webp')] absolute inset-0 bg-cover bg-bottom">
+      <div className="bg-[url('/images/hero.webp')] absolute bg-cover bg-bottom">
+        <div className="w-screen h-screen flex">
+          <div className="flex flex-col justify-start items-center w-full h-full p-4 m-4">
+          <h1 className="text-4xl text-white font-bold">Bienvenido a La Velada del Año V</h1>
+          <p className="text-lg text-white mt-4">La velada del Año V es un evento de boxeo benéfico organizado por Ibai Llanos, donde creadores de contenido se enfrentan en el ring para recaudar fondos para diversas causas.</p>
+          <p className="text-lg text-white mt-4">Disfruta de la emoción, la rivalidad y el espectáculo en un evento que une a la comunidad de creadores.</p>
+          <p className="text-lg text-white mt-4">¡No te pierdas la acción y acompáñanos en esta increíble velada!</p>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
