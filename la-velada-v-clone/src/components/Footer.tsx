9f0d33906efb41a1e27c0ab12ca6ce1576ac328b
RicardoEA <ricardoea@hotmail.es>
2025-06-13 19:00:41 +0100

    feat: ajustar estilos y estructura del pie de página

--- la-velada-v-clone/src/components/Footer.tsx
+++ la-velada-v-clone/src/components/Footer.tsx
@@ -10,26 +10,26 @@ import Ti from "@/assets/svg/tiktok.svg"
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-screen h-[400px]">
+    <footer className="fixed bottom-0 left-0 w-screen h-[300px] z-40">
       <div
         className="absolute inset-0 bg-cover bg-bottom bg-[url('/images/footer.webp')] [mask-image:linear-gradient(transparent,black_50%)]"
       >
       </div>
 
       <div
-        className="relative h-full flex justify-center items-center text-white text-center "
+        className="relative h-full w-full flex flex-col"
       >
 
-        <div className="flex flex-col justify-start items-center w-full h-full">
+        <div className="justify-center items-center w-full h-auto flex flex-col gap-4">
 
-          <h1
-            className="text-center text-lg text-[#2a1f26]"
-          >
-            © 2025 La Velada del Año. Todos los derechos reservados.
+            <h1
+              className="text-center text-lg text-[#f7d8fd]"
+            >
+              © 2025 La Velada del Año. Todos los derechos reservados.
             </h1>
 
 
-          <div className=" flex flex-row gap-3 justify-center items-center text-[#2a1f26] h-32">
+          <div className="w-auto flex gap-3 justify-center items-center text-[#f7d8fd]">
 
             <Link href="http://X.com/infoLaVelada" rel="noopener"
               className="w-8 h-8 flex justify-center items-center hover:scale-125 transition duration-300 hover:cursor-pointer"
@@ -57,18 +57,15 @@ export const Footer: React.FC = () => {
 
           </div>
         </div>
-        <div className="w-full absolute bottom-16 items-center justify-center ">
-          <div className="w-2/5 gap-8 flex flex-col justify-center items-center">
-            <h1 className="text-center text-xl text-[#2a1f26]">
-              Patrocinadores del evento
-            </h1>
+        <div className="w-full items-center justify-center lg:flex-row gap-8 h-auto">
+          <div className="w-auto gap-8 justify-center items-center lg:w-2/5">
             <div>
               <Sponsors />
             </div>
           </div>
         </div>
-        <div className="absolute bottom-0 left-0 w-full flex justify-center items-end h-12">
-          <p className="text-center text-sm text-[#2a1f26]">
+        <div className="w-full justify-center items-center flex flex-col h-full">
+          <p className="text-sm text-[#f7d8fd]">
             Hecho con ❤️ por RiesalDev siguiendo el diseño de la web oficial de La Velada del Año V realizada por Midudev y la comunidad.
           </p>
         </div>
