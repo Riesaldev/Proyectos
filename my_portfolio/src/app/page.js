cbcc603d6315251b9c9bc6e533befa9bd6a6f9b7
RicardoEA <ricardoea@hotmail.es>
2025-06-26 16:46:42 +0100

    Refactor homepage: remove turbopack from dev script, add video component with play/pause functionality, and include new video asset.

--- my_portfolio/src/app/page.js
+++ my_portfolio/src/app/page.js
@@ -1,57 +1,60 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import PreloadPage from "./precarga/PreloadPage";
 import LavenderFog from "../components/Three/LavenderFog";
+import VideoSource from "../../public/assets/videos/puerta.mp4";
 
-export default function Home() {
-  const [showHome, setShowHome] = useState(false);
-  const [startScroll, setStartScroll] = useState(false);
+export default function Home () {
+  const [ showHome, setShowHome ] = useState( false );
+  const videoRef = useRef(null); // Definimos el videoRef
 
-  const handleScrollClick = () => {
-    setStartScroll(true);
-    setTimeout(() => {
-      window.scrollBy({ top: 400, behavior: "smooth" });
-    }, 300);
+  const handleVideoClick = () => {
+    // Usamos la referencia al elemento video DOM
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
   };
 
   return (
     <>
-      {!showHome && <PreloadPage onContinue={() => setShowHome(true)} />}
+      {!showHome && <PreloadPage onContinue={() => setShowHome( true )} />}
       {showHome && (
         <main
-        className={`flex flex-col items-center h-screen p-24 transition-colors duration-700 ${
-          startScroll
-          ? "bg-black"
-          : "bg-gradient-to-b from-[#a842b1] from-70% to-[#2c002e]"
-          } text-[#fddbff]`}
-          >
+          className="flex flex-col items-center h-screen overflow-hidden bg-[#a842b1] text-[#fddbff]"
+        >
           <LavenderFog />
-          <div className="md:grid grid-cols-2 items-center justify-center w-full h-full p-6 my-8">
+          <div className="md:grid grid-cols-2 items-center justify-center w-full h-full p-6">
             {/* Letras y botón */}
-            <div className={`text-center md:text-left md:pl-12 transition-opacity duration-500 ${
-                startScroll ? "opacity-0 pointer-events-none" : "opacity-100"
-              }`}>
+            <div className="text-center md:text-left md:pl-12">
               <h1 className="text-4xl font-bold pb-4">Bienvenido a mi "Mundo"</h1>
               <p className="mt-4 text-lg">
                 Soy un desarrollador web con pasión por crear sitios web hermosos y funcionales aunando diseño, tecnología, creatividad y por qué no... ¡Magia! Espero que disfruten de esta experiencia tanto como a mí me ha encantado hacerla.
               </p>
             </div>
-            {/* Logo */}
-            <div className="flex items-center justify-center mt-10">
-              <img src="/logo1.svg" alt="Logo" className="h-62 w-auto min-w-96" />
-            </div>
-            {/* Botón */}
-            <div className={`text-center md:text-left md:pl-12 mt-10 transition-opacity duration-500 ${
-                startScroll ? "opacity-0 pointer-events-none" : "opacity-100"
-              }`}>
-              <button
-                type="button"
-                aria-label="Scroll to bottom"
-                onClick={handleScrollClick}
-                className="bg-[#5a0856] text-[#fddbff] font-bold py-2 px-4 hover:shadow-fuchsia-400 shadow-md rounded-2xl hover:bg-[#670b7a] transition-all duration-300"
+            {/* Video*/}
+            <div className="relative w-full max-w-2xl mx-auto">
+              {/* Video con máscara aplicada para crear el efecto de desvanecimiento en los bordes */}
+              <div
+                className="w-full"
+                style={{
+                  WebkitMaskImage: 'radial-gradient(ellipse, black 55%, transparent 70%)',
+                  maskImage: 'radial-gradient(ellipse, black 55%, transparent 70%)'
+                }}
               >
-                Sigamos adelante !!
-              </button>
+                <video
+                  ref={videoRef}
+                  onClick={handleVideoClick}
+                  className="w-full cursor-pointer"
+                  playsInline
+                >
+                  <source src={VideoSource} type="video/mp4" />
+                  Tu navegador no soporta el elemento video.
+                </video>
+              </div>
             </div>
           </div>
         </main>
