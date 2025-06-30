36325d1e8acb71fb724619ce339c5536d1c46050
RicardoEA <ricardoea@hotmail.es>
2025-06-30 20:52:13 +0100

    fix: corregir capitalización en el texto y actualizar la imagen de flecha en el menú

--- my_portfolio/src/app/menu/page.js
+++ my_portfolio/src/app/menu/page.js
@@ -22,7 +22,7 @@ export default function Menu()  {
       </div>
       <div className="absolute text-center flex flex-col items-center justify-center top-24">
         <h1 className="text-4xl font-extrabold text-[#8653a8]">Bienvenido a la sala de los portales!!</h1>
-        <p className="text-lg text-[#fddbff] w-1/2">Desde esta sala puedes acceder a diferentes portales y explorar nuevas dimensiones en busca de la "espada del destino" capaz de traer la paz al multiverso y felicidad a sus habitantes.</p>
+        <p className="text-lg text-[#fddbff] w-1/2">Desde esta sala puedes acceder a diferentes portales y explorar nuevas dimensiones en busca de "la Espada del Destino" capaz de traer la paz al multiverso y felicidad a sus habitantes.</p>
         <span className="mt-4 text-[#fddbff] text-lg">Pulsa en un portal para continuar tu aventura</span>
       </div>
       <div className="absolute bottom-1/2  flex space-x-54 items-center justify-center">
@@ -30,7 +30,7 @@ export default function Menu()  {
         <div className="Portal flex flex-col items-center justify-center">
         <p className="text-[#812286] text-xl font-black text-center justify-around items-center relative bottom-6">Portal del Lago <br/><span>(Proyectos)</span></p>
         </div>
-        <img src="/assets/images/arrow3.png" alt="Flecha" className="w-24 h-24 rotate-180 top-40 relative" />
+        <img src="/assets/images/arrow1.png" alt="Flecha" className="w-24 h-24 top-40 relative" />
       </div>
     </div>
   );
