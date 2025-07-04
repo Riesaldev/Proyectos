cf6863ff71c7b0adf4beb306c71d6de830f929c0
RicardoEA <ricardoea@hotmail.es>
2025-07-04 12:55:09 +0100

    feat: agregar páginas de About, Contact, Projects y Menu con funcionalidad de precarga de videos

---
+++ my_portfolio/src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+
+
+
+
+export default function Page () {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
---
+++ my_portfolio/src/app/proyects/page.jsx
@@ -0,0 +1,12 @@
+
+
+
+
+export default function Page () {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>This is the projects page.</p>
+    </div>
+  );
+}
