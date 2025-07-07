7f2d798d53f15b190db1b8033e4cdce6c450d2e7
RicardoEA <ricardoea@hotmail.es>
2025-07-07 20:36:43 +0100

    fix: corregir formato de importaciones y eliminar etiquetas innecesarias en layout

--- my_portfolio/src/app/layout.js
+++ my_portfolio/src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -16,15 +16,13 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "My Portfolio",
   description: "A showcase of my work and skills as a web developer.",
+  icons: "/assets/logo2.svg"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💼</text></svg>" />
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}antialiased`}>
         {children}
       </body>
     </html>
