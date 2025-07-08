061c048ed2eec31a5ce3ee227b4110a5a54f95a6
RicardoEA <ricardoea@hotmail.es>
2025-07-08 13:58:58 +0100

    feat: agregar componente ContactForm y actualizar contenido de los pergaminos

--- my_portfolio/src/app/contact/page.jsx
+++ my_portfolio/src/app/contact/page.jsx
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from "react";
 import Header from "@/components/Header";
 import Sword from "../../../public/videos/Sword.webm";
 import Ancient from "@/components/Ancient";
+import ContactForm from "@/components/ContactForm";
 
 export default function Page () {
   const [ videoSpeed, setVideoSpeed ] = useState( 0.5 );
@@ -31,19 +32,17 @@ export default function Page () {
   const scrollContents = [
     {
       title: "El Bosque de Las Hadas",
-      content: "<p>Tras incontables días de investigación, sumergido en cada libro a su alcance, el Mago por fin ha descubierto la manera de abrir un portal al mundo de los sueños, donde habita la misteriosa Dama del Lago.</p> " +
-        "<p>Con la ayuda de su fiel asistente, ha preparado todos los ingredientes necesarios para llevar a cabo el ritual.</p> " +
-        "<p>El laboratorio rebosa de frascos, pociones y artefactos mágicos, cada uno con una función específica dentro de este proceso arcano.</p>" +
-        "<p>En el aire se respira una mezcla de misterio y expectación mientras ambos se disponen a dar el siguiente paso en su viaje onírico, con la esperanza de encontrar la Espada del Destino y, con ella, la salvación de su mundo.</p>",
+      content: "<p>El Bosque de Las Hadas es un lugar de paz y armonía, donde la naturaleza y la magia coexisten en perfecta sintonía. Aquí, las criaturas mágicas viven en perfecta armonía, cuidando de su hogar y protegiendo los secretos que guarda. Es un lugar donde los sueños se hacen realidad y las leyendas cobran vida.</p>"+
+      "<p>En lo más profundo del bosque, donde la luz apenas penetra, se encuentra un lugar mágico donde las hadas danzan y juegan entre los árboles. Este es un mundo de ensueño, lleno de colores vibrantes y sonidos melodiosos. En este lugar, en el que tiempo parece detenerse y la realidad se mezcla con la fantasía existe un claro bañado por el sol donde, finalmente, nuestro héroes encuentran la Espada del Destino y con ella la clave para salvar a todos los seres del multiverso.</p>" +
+      "<p>FIN.</p>",
     },
     {
-      title: "Sobre mi",
-      content: "<p>Soy un desarrollador web apasionado por la tecnología, el diseño y la narrativa interactiva.</p>"+
-      "<p>Mi vocación es crear experiencias únicas que no solo sean funcionales, sino también visualmente impactantes y emocionalmente significativas.</p>"+
-      "<p>Aunque mi experiencia laboral comenzó en 2004, fue en 2018 cuando inicié un camino de formación en modelado 3D, especializándome en herramientas como Blender, ZBrush y 3ds Max.</p>"+
-      "<p>Cursé estudios como Técnico Superior en Escultura, pero por giros inesperados de la vida, terminé dedicándome a la construcción como soldador y montador de estructuras metálicas. Sin embargo, esa etapa me llevó a replantear mi rumbo.</p>"+
-      "<p>Decidí dar un giro de 180 grados y reenfocar mi futuro en lo que realmente me apasiona: la tecnología y la creatividad. En 2024 me formé como desarrollador web full stack a través de un bootcamp intensivo, complementando mi aprendizaje con certificaciones en frameworks de JavaScript y CSS, TypeScript, y metodologías ágiles como Scrum. Hoy fusiono toda esa experiencia para construir proyectos que cuentan historias, despiertan emociones y dan vida a ideas.</p>"+
-      "<p>Aunque mi experiencia profesional en desarrollo web es reciente, mi pasión por la tecnología y el diseño es de larga data y suplo con mi dedicación y creatividad lo que me falta en años de experiencia. También aporto mi experiencia previa en el ámbito de la construcción, la hostelería (12 años en puestos de responsabilidad en cocina y eventos; desde trabajar en el World Travel Market de Londres a ser jefe de sección en hoteles de 5 estrellas gran lujo o aprender de mano de estrellas Michelin y trabajar con ellos) e incluso en el Ejercito Español, donde aprendí disciplina, trabajo en equipo y liderazgo. Todo ello me ha permitido desarrollar una mentalidad resiliente y una capacidad de adaptación que considero esenciales en el mundo del desarrollo web.</p>",
+      title: "Contacto",
+      content: "<p>Si deseas ponerte en contacto conmigo, puedes hacerlo a través del siguiente formulario:</p>"+
+      "<p class='mt-4'>¡Espero que disfrutes de mi trabajo!</p>"+
+      "<p class='mt-4 text-fuchsia-600 underline'><a href='/farewell'>Agradecimientos</a></p>",
+      hasCustomComponent: true,
+      customComponent: "ContactForm"
     },
     
   ];
@@ -119,6 +118,7 @@ export default function Page () {
     };
   }, [] );
 
+  // Modificar el componente Ancient para renderizar componentes personalizados
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <div className="cover absolute inset-0 z-0">
@@ -167,6 +167,7 @@ export default function Page () {
                 onChangePage={changePage}
                 onGoToPage={goToPage}
                 transitionActive={transitionActive}
+                customComponent={scrollContents[currentPage].hasCustomComponent ? <ContactForm /> : null}
               />
             </div>
           </div>
